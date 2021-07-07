c1936084f24423f15354beaad40e74c4a4fe1d02
ziggh28 <haiderzouhrab.1128@gmail.com>
2021-07-07 16:27:55 -0400

    fixed

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const game ={
 Title:"guess the Numeber",
 biggestNum: 100,
 smallestNum:1,
-secretNum: 1,
+secretNum:1,
 guessesByplayer: null,
 prevGuesses: [],
 play:function(){
-// this.secretNum = Math.floor(Math.random()*(this.biggestNum - this.smallestNum + 1))+this.smallestNum
+this.secretNum = Math.floor(Math.random()*(this.biggestNum - this.smallestNum + 1))+this.smallestNum
 console.log(this.secretNum)
 game.getGuesses()
 
@@ -17,13 +17,13 @@ getGuesses: function(){
     this.guessesByplayer = (parseInt(prompt("guess a number between 1 and 100"),console.log(this.guessesByplayer)))
      
     if(this.guessesByplayer === this.secretNum){
-          alert(`${this.guessesByplayer} congratulations you guess`)}
+          alert(`${this.guessesByplayer} congratulations you guess correctly`)}
             
-          else if (this.guessesByplayer > this.secretNum){
+          else if (this.guessesByplayer < this.secretNum){
             alert(`${this.guessesByplayer} your guess was too low`)
           }
             
-          else if(this.guessesByplayer < this.secretNum){
+          else if(this.guessesByplayer > this.secretNum){
                 alert(`${this.guessesByplayer} your guess was too high`)
               
               }
